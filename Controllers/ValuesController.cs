87e0ddc5e9579c86006037766bc7b7de9c25c379
benmp <ben@DESKTOP-D6ACKUT.localdomain>
2017-06-23 16:27:00 -0500

    original push with base api

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TwitchWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private IRedisClient _redisClient;
+        public ValuesController(IRedisClient redisClient)
+        {
+            _redisClient = redisClient;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { _redisClient.RedisServer.Configuration };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public int Get(int id)
+        {
+            return id;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace TwitchWebAPI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<IRedisClient, RedisClient>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+    }
+
+    public interface IRedisClient
+    {
+        StackExchange.Redis.ConnectionMultiplexer RedisServer { get; set; }
+    }
+
+    public class RedisClient : IRedisClient
+    {
+        public RedisClient()
+        {
+            try{
+                RedisServer = StackExchange.Redis.ConnectionMultiplexer.Connect("localhost");
+            }
+            catch{
+                Console.WriteLine("Redis failed to connect");
+            }
+        }
+        public StackExchange.Redis.ConnectionMultiplexer RedisServer { get; set; }
+    }
+}
